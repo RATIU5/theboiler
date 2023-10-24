9824db396db919db296cc623fc635bb49ebf6387
RATIU5 <jcmemmott20@outlook.com>
2023-10-24 17:47:50 -0600

    refactor: root command

--- cmd/root.go
+++ cmd/root.go
@@ -2,16 +2,25 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
-var Root = &cobra.Command{
-	Use:   "boil [cmd]",
-	Short: "Run boil command",
+var root = &cobra.Command{
+	Use:   "boil",
+	Short: "Boilerplate and template manager",
+	Long:  "A fancy boilerplate and template manager for all your development needs",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("TheBoiler: boilerplate and template management with ease")
 		fmt.Printf("Type `%v` for a list of commands\n", color.GreenString("boil help"))
 	},
+}
+
+func Execute() {
+	if err := root.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/RATIU5/theboiler/cmd"
 
 func main() {
-	cmd.Root.Execute()
+	cmd.Execute()
 }
